da95d1e816fb80328ba281580cfc25a093b16c71
Charles Myers <55416416+cmyers18uwyo@users.noreply.github.com>
2021-03-09 20:44:14 -0700

    Add files via upload

--- wyls.c
+++ wyls.c
@@ -140,7 +140,8 @@ int main(int argc, char* argv[]) {
 						}
 						else {
 							if(stat(argv[i],&stats) == 0) {
-								//fileOutput(stats, de, pwd, grp, time, nFlag, hFlag, dates);
+								fileOutput(stats, de, pwd, grp, time, nFlag, hFlag, dates);
+								/*
 								printf((S_ISDIR(stats.st_mode)) ? "d" : "-");
 								printf((stats.st_mode & S_IRUSR) ? "r" : "-");
 								printf((stats.st_mode & S_IWUSR) ? "w" : "-");
@@ -204,7 +205,7 @@ int main(int argc, char* argv[]) {
 
 								printf("\n");
 								
-								
+								*/
 								//a
 							}
 						}
@@ -221,7 +222,8 @@ int main(int argc, char* argv[]) {
 			if(de->d_name[0] != '.') {
 				//strcat(path,de->d_name);
 				if(stat(de->d_name,&stats) == 0) {
-					//fileOutput(stats, de, pwd, grp, time, nFlag, hFlag, dates);
+					fileOutput(stats, de, pwd, grp, time, nFlag, hFlag, dates);
+					/*
 					printf((S_ISDIR(stats.st_mode)) ? "d" : "-");
 					printf((stats.st_mode & S_IRUSR) ? "r" : "-");
 					printf((stats.st_mode & S_IWUSR) ? "w" : "-");
@@ -284,6 +286,7 @@ int main(int argc, char* argv[]) {
 					printf("%s",de->d_name);
 
 					printf("\n");
+					*/
 				}
 			}
 		}
@@ -292,67 +295,67 @@ int main(int argc, char* argv[]) {
 }
 
 void fileOutput(struct stat stats, struct dirent *de, struct passwd *pwd, struct group *grp, struct tm time, int nFlag,int hFlag,char dates[]) {
-	printf((S_ISDIR(stats.st_mode)) ? "d" : "-");
-	printf((stats.st_mode & S_IRUSR) ? "r" : "-");
-	printf((stats.st_mode & S_IWUSR) ? "w" : "-");
-	printf((stats.st_mode & S_IXUSR) ? "x" : "-");
-	printf((stats.st_mode & S_IRGRP) ? "r" : "-"); 
-	printf((stats.st_mode & S_IWGRP) ? "w" : "-");
-	printf((stats.st_mode & S_IXGRP) ? "x" : "-");
-	printf((stats.st_mode & S_IROTH) ? "r" : "-");
-	printf((stats.st_mode & S_IWOTH) ? "w" : "-");
-	printf((stats.st_mode & S_IXOTH) ? "x" : "-");
+printf((S_ISDIR(stats.st_mode)) ? "d" : "-");
+printf((stats.st_mode & S_IRUSR) ? "r" : "-");
+printf((stats.st_mode & S_IWUSR) ? "w" : "-");
+printf((stats.st_mode & S_IXUSR) ? "x" : "-");
+printf((stats.st_mode & S_IRGRP) ? "r" : "-"); 
+printf((stats.st_mode & S_IWGRP) ? "w" : "-");
+printf((stats.st_mode & S_IXGRP) ? "x" : "-");
+printf((stats.st_mode & S_IROTH) ? "r" : "-");
+printf((stats.st_mode & S_IWOTH) ? "w" : "-");
+printf((stats.st_mode & S_IXOTH) ? "x" : "-");
+printf(" ");
+if(nFlag == 1) {
+	printf("%d", stats.st_uid);
 	printf(" ");
-	if(nFlag == 1) {
-		printf("%d", stats.st_uid);
-		printf(" ");
-		printf("%d", stats.st_gid);
-	}
-	else {
-		pwd = getpwuid(stats.st_uid);
-		grp = getgrgid(stats.st_gid);
-		printf("%s", pwd->pw_name);
-		printf(" ");
-		printf("%s", grp->gr_name);
-	}
+	printf("%d", stats.st_gid);
+}
+else {
+	pwd = getpwuid(stats.st_uid);
+	grp = getgrgid(stats.st_gid);
+	printf("%s", pwd->pw_name);
 	printf(" ");
+	printf("%s", grp->gr_name);
+}
+printf(" ");
 
-	if(hFlag == 1) {
-		float byteSize;
-		long statsSize = stats.st_size;
-		if(statsSize >= 1024) {
-			byteSize = stats.st_size / 1024;
-			printf ("%.1f",byteSize);
-			printf ("K");
-		}
-		else if(statsSize >= 1048576) {
-			byteSize = stats.st_size / 1048576;
-			printf ("%.1f",byteSize);
-			printf ("M");
-		}
-		else if(statsSize >= 1073741824) {
-			byteSize = stats.st_size / 1073741824;
-			printf ("%.1f",byteSize);
-			printf ("G");
-		}
-		else {
-			printf ("%ld",stats.st_size);
-		}
-		
+if(hFlag == 1) {
+	float byteSize;
+	long statsSize = stats.st_size;
+	if(statsSize >= 1024 && statsSize <= 1048576) {
+		byteSize = stats.st_size / 1024;
+		printf ("%.1f",byteSize);
+		printf ("K");
+	}
+	else if(statsSize >= 1048576 && statsSize <= 1073741824) {
+		byteSize = stats.st_size / 1048576;
+		printf ("%.1f",byteSize);
+		printf ("M");
+	}
+	else if(statsSize >= 1073741824) {
+		byteSize = stats.st_size / 1073741824;
+		printf ("%.1f",byteSize);
+		printf ("G");
 	}
 	else {
 		printf ("%ld",stats.st_size);
 	}
-	printf(" ");
 
-	localtime_r(&stats.st_mtime, &time);
-	strftime(dates, sizeof(dates), "%b %A %Y", &time);
-	printf("%s%d",dates);
-											   
-	printf(" ");
+}
+else {
+	printf ("%ld",stats.st_size);
+}
+printf(" ");
 
-	printf("%s",de->d_name);
+localtime_r(&stats.st_mtime, &time);
+strftime(dates, sizeof(dates), "%b %d %Y", &time);
+printf("%s",dates);
+										
+printf(" ");
 
-	printf("\n");
-	
+printf("%s",de->d_name);
+
+printf("\n");
+
 }
